83b7328c500e646c500b198d1b9c1329be05da5f
Nishita <nishitakarda4@gmail.com>
2025-07-05 18:04:05 +0530

    updated

--- server.js
+++ server.js
@@ -47,9 +47,9 @@ app.use("/api/messages", messageRouter);
 //connect to MongoDB
 await connectDB()
 
-if(process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 5000;
-    server.listen(PORT, ()=>console.log("Server is running on PORT: " + PORT));
-}
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=>console.log("Server is running on PORT: " + PORT));
+
  // exprt server for vercel
 export default server;
